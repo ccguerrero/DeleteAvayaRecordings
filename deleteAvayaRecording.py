dd485e136909efe1a26748136db7e26ed7351900
Cuauhtemoc Guerrero <ccguerrero@gmail.com>
2024-01-03 00:15:49 -0800

    Update logging level and add recording deletion count.

--- deleteAvayaRecording.py
+++ deleteAvayaRecording.py
@@ -12,8 +12,9 @@ username = "FMEX000011"
 password = "W9W9Kv£E*M7mt}7~"
 attempt = 0  # Initialize the attempt variable
 max_attempts = 3  # Define the maximum number of attempts
+deleted_count = 0  # Initialize counter
 
-logging.basicConfig(filename='deleteAvayaRecording.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.basicConfig(filename='deleteAvayaRecording.log', level=logging.INFO, format='%(asctime)s %(message)s')
 
 options = Options()
 # Use defult profile on Chrome
@@ -24,16 +25,18 @@ driver.implicitly_wait(10)
 
 # Login to Avaya
 # Use the url for the required search parameters to delete recordings
-driver.get("https://wfo-app.glb.nar.fusion.avayacloud.com/wfo/ui/#wsm%5Bws%5D=qm_SearchResultsWorkspace&navparent%5BworkspaceId%5D=qm_SearchWorkspace&qm_ctx%5Bts%5D=1703921082176")
+driver.get("https://wfo-app.glb.nar.fusion.avayacloud.com/wfo/ui/#wsm%5Bws%5D=qm_SearchResultsWorkspace&navparent%5BworkspaceId%5D=qm_SearchWorkspace&qm_ctx%5Bts%5D=1704231992664")
 driver.find_element(By.ID, "username").send_keys(username)  # use By.ID
 driver.find_element(By.CLASS_NAME, "loginButtonLabel").click()
+logging.info("Username entered")
 driver.find_element(By.ID, "password").send_keys(password)  # use By.ID
 driver.find_element(By.CLASS_NAME, "loginButtonLabel").click()
+logging.info("Password entered")
 
 # Run until no recordings found
 while True:
     try:
-        # Try to find the element
+        # Try to find the number of recordings left on search
         element = driver.find_element(By.XPATH, '//*[contains(@class, "verint-search-label-found")]')
         # If the element's text is 'Retrieved 0 items', break the loop
         if element.text == 'Retrieved 0 items':
@@ -45,24 +48,32 @@ while True:
             #driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div/a[4]/span/span/span[1]').click()
             for attempt in range(max_attempts):
                 try:
-                    wait = WebDriverWait(driver, 5)
+                    wait = WebDriverWait(driver, 1)
                     element1 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div/a[4]/span/span/span[1]')))
-                    time.sleep(6)
+                    time.sleep(4)
                     element1.click()
-                    logging.debug("Delete button found")
-                    element2 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span')))# wait for yes dialog box
-                    element2.click()
-                    logging.debug("Recording deleted")
+                    logging.info("Delete button found")
+                    #element2 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span')))# click for yes dialog box
+                    try:
+                        element2 = driver.find_element(By.XPATH, '/html/body/div[22]/div[4]/div/div/a[2]/span/span/span[2]')
+                        element2.click()
+                    except NoSuchElementException:
+                        driver.find_element(By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span/span[2]').click() #click yes dialog box rest of times
+                        #element2.click()
+                    deleted_count += 1  # Increment counter
+                    logging.info(f"Recording deleted {deleted_count}")
                     #driver.find_elements(By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span') #click yes dialog box
                     break
                 except TimeoutException:
                     logging.error("Timeout while waiting for the element to be clickable, attempt: %s", attempt + 1)
                     if attempt + 1 == max_attempts:
                         raise  # If this was the last attempt, re-raise the exception
+                    logging.info(f"Total recordings deleted: {deleted_count} (Time out)")
                 except NoSuchElementException:
-                    logging.debug("Delete button not found, returning to search results")
+                    logging.info("Delete button not found, returning to search results")
                     driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[1]/div/div/div/div/div/a[2]/span/span/span[2]').click() #click back to search results
                     break
     except NoSuchElementException:
         # If the element is not found, wait for a second and then continue the loop
         break
+logging.info(f"Total recordings deleted: {deleted_count} (end)")
