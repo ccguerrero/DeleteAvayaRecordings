82b3157bb421c073d83678f00ee3008007aa4e2e
Cuauhtemoc Guerrero <ccguerrero@gmail.com>
2024-01-19 00:16:59 -0800

    Handles exception when not delete button found. Validate 3 different elements for confirmation of deletion.

--- deleteAvayaRecording.py
+++ deleteAvayaRecording.py
@@ -24,7 +24,7 @@ driver.implicitly_wait(10)
 
 # Login to Avaya
 # Use the url for the required search parameters to delete recordings
-driver.get("https://wfo-app.glb.nar.fusion.avayacloud.com/wfo/ui/#wsm%5Bws%5D=qm_SearchResultsWorkspace&navparent%5BworkspaceId%5D=qm_SearchWorkspace&qm_ctx%5Bts%5D=1703921082176")
+driver.get("https://wfo-app.glb.nar.fusion.avayacloud.com/wfo/ui/#wsm%5Bws%5D=qm_SearchResultsWorkspace&navparent%5BworkspaceId%5D=qm_SearchWorkspace&qm_ctx%5Bts%5D=1705640819247")
 driver.find_element(By.ID, "username").send_keys(username)  # use By.ID
 driver.find_element(By.CLASS_NAME, "loginButtonLabel").click()
 driver.find_element(By.ID, "password").send_keys(password)  # use By.ID
@@ -42,29 +42,38 @@ while True:
             logging.info("No recordings found")
             break
         else:
-            time.sleep(3)
+            time.sleep(2)
             driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div[1]/div/div/div[6]/div/div').click()
             #driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div/a[4]/span/span/span[1]').click()
             for attempt in range(max_attempts):
                 try:
-                    wait = WebDriverWait(driver, 5)
-                    element1 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div/a[4]/span/span/span[1]')))
-                    time.sleep(6)
-                    element1.click()
-                    logging.debug("Delete button found")
-                    element2 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span')))# wait for yes dialog box
-                    element2.click()
-                    logging.debug("Recording deleted")
-                    #driver.find_elements(By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span') #click yes dialog box
+                    # Click the first element
+                    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div/a[4]/span/span/span[1]').click()
+                    try:
+                        # Try to click the first of the two elements
+                        driver.find_element(By.XPATH, '/html/body/div[22]/div[4]/div/div/a[2]/span/span/span[2]').click()
+                        logging.info("Recording deleted")
+                        break
+                    except NoSuchElementException:
+                        try:
+                            # If the first element is not found, try to click the second element
+                            driver.find_element(By.XPATH, '/html/body/div[20]/div[4]/div/div/a[2]/span/span').click()
+                            logging.info("Recording deleted")
+                            break
+                        except NoSuchElementException:
+                            # If the second element is not found, try to click the third element
+                            driver.find_element(By.XPATH, '/html/body/div[21]/div[4]/div/div/a[2]/span/span/span[2]').click()
+                            logging.info("Recording deleted")
+                            break
+                except NoSuchElementException:
+                    # If neither element is found, return to search results
+                    logging.error("Delete button not found, returning to search results")
+                    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[1]/div/div/div/div/div/a[2]/span/span/span[2]').click()
                     break
                 except TimeoutException:
                     logging.error("Timeout while waiting for the element to be clickable, attempt: %s", attempt + 1)
                     if attempt + 1 == max_attempts:
-                        raise  # If this was the last attempt, re-raise the exception
-                except NoSuchElementException:
-                    logging.debug("Delete button not found, returning to search results")
-                    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[1]/div/div/div/div/div/a[2]/span/span/span[2]').click() #click back to search results
-                    break
+                        raise  # If this was the last attempt, re-raise the exception so that the script fails          
     except NoSuchElementException:
         # If the element is not found, wait for a second and then continue the loop
         break
